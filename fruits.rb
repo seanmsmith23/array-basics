4374111b8b30c7c14f6f77cc20d216775165bdde
Sean Smith <seansmith1020@gmail.com>
2014-06-17 13:21:03 -0600

    Adding after redoing fruits.rb excercise solo

--- fruits.rb
+++ fruits.rb
@@ -1,1 +1,30 @@
 fruits = ['apples', 'oranges', 'bananas', 'pomegranates', 'grapes']
+
+puts "Printing array length..."
+puts fruits.length
+
+puts "Printing the index of 'bananas'..."
+puts fruits.index('bananas')
+
+copied_fruits = fruits.dup
+
+puts "Inserting raspberries..."
+added_raspberries = copied_fruits.insert(1, 'raspberries')
+puts added_raspberries
+
+puts "Printing the character length of each fruit..."
+fruits.each do |item|
+  puts item.length
+end
+
+puts "Upcasing each fruit..."
+fruits.each do |item|
+  puts item.upcase
+end
+
+puts "Only printing fruits that contain the letter 'g'..."
+fruits.each do |item|
+  if item.include?('g')
+    puts item
+  end
+end
