0acbe4a2709cbdd77275f494fc7d4a678ecca188
Sean Smith <seansmith1020@gmail.com>
2014-06-17 14:39:24 -0600

    Adding a friends list which pushes friend name and the length of name into an individual sub-array

---
+++ friends_nested_array.rb
@@ -0,0 +1,16 @@
+list_of_friends = []
+
+puts "Please enter the names of your friends. When you are finished just hit 'ENTER' and we will display your friends."
+still_adding_friends = true
+
+while still_adding_friends
+  puts "Name:"
+  response = gets.chomp.capitalize
+
+  if response == ""
+    print list_of_friends
+    still_adding_friends = false
+  else
+    list_of_friends.push([response, response.length])
+  end
+end
